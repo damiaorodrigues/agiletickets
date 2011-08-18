feda1601751106e076aed4a5f623fab24c8e381a
Damiao Rodrigues Oliveira <damiaorodrigues.oliveira@gmail.com>
2011-08-18 12:37:26 -0400

    Retificacao da classe SessaoTest

--- src/test/java/br/com/caelum/agiletickets/models/SessaoTest.java
+++ src/test/java/br/com/caelum/agiletickets/models/SessaoTest.java
@@ -1,30 +1,34 @@
 package br.com.caelum.agiletickets.models;
 
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 public class SessaoTest {
-	
-	Sessao sessao;
-	
+
+	private Sessao sessao;
+
 	@Before
 	public void setUp() {
-		sessao = new Sessao();
+		this.sessao = new Sessao();
 	}
-	
+
+	@After
+	public void depois() {
+
+	}
 
 	@Test
 	public void deveVender1ingressoSeHa1vagas() throws Exception {
-        sessao.setTotalIngressos(1);
-        Assert.assertTrue(sessao.podeReservar(1));
+		sessao.setTotalIngressos(1);
+		Assert.assertTrue(sessao.podeReservar(1));
 	}
-	
-	
+
 	@Test
 	public void deveVender1ingressoSeHa2vagas() throws Exception {
-        sessao.setTotalIngressos(2);
-        Assert.assertTrue(sessao.podeReservar(1));
+		sessao.setTotalIngressos(2);
+		Assert.assertTrue(sessao.podeReservar(1));
 	}
 
 	@Test
@@ -34,7 +38,8 @@ public class SessaoTest {
 	}
 
 	@Test
-	public void reservarIngressosDeveDiminuirONumeroDeIngressosDisponiveis() throws Exception {
+	public void reservarIngressosDeveDiminuirONumeroDeIngressosDisponiveis()
+			throws Exception {
 		sessao.setTotalIngressos(5);
 		sessao.reserva(3);
 		Assert.assertEquals(2, sessao.getIngressosDisponiveis().intValue());
